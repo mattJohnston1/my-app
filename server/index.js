455bfc2854e524d5132bd47999cb1d76cbdf998c
emollientspy1 <emollientspy1@gmail.com>
2022-02-01 13:10:58 -0700

    initial

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const axios = require('axios')
+
+
+const app = express();
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
+
+app.get('/', (req, res) => {
+  axios.get('https://coffee.alexflipnote.dev/random.json')
+    .then((response) => {
+      if (response.status == 200) {
+        res.status(200)
+        res.json(response.data.file)
+      } else {
+        throw new Error('Oops! Something went wrong')
+      }
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+})
+
+app.listen(3000, () => {
+  console.log('server started');
+});
